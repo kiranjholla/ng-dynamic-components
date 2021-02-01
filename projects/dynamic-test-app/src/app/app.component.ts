ffda4fa7b0f3a5bb87d99bff67297c30c36af4d1
Kiran J. Holla <kjholla@gmail.com>
2021-02-01 07:56:42 +0530

    Modify the AppComponent to use the Dynamic Components

--- projects/dynamic-test-app/src/app/app.component.ts
+++ projects/dynamic-test-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { ComponentData } from 'ng-dynamic-components';
+
 @Component({
   selector: 'dta-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dynamic-test-app';
+  comps: ComponentData[] = [
+    { name: 'OneComponent', data: { message: 'Some Message for One' } },
+    { name: 'TwoComponent', data: { message: 'Some Message for Two' } },
+    { name: 'ThreeComponent', data: { message: 'Some Message for Three' } }
+  ];
 }
