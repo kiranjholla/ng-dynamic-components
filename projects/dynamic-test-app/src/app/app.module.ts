470c88a4ed6e31eefdd6579316f295bb2c0b7507
Kiran J. Holla <kjholla@gmail.com>
2021-02-01 07:58:54 +0530

    Wiring in the NgDynamicComponents module into the Test App with the Dynamic Components

--- projects/dynamic-test-app/src/app/app.module.ts
+++ projects/dynamic-test-app/src/app/app.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, Compiler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  componentRegistryInitializerFactory,
+  ComponentRegistryService,
+  DynamicComponents,
+  NgDynamicComponentsModule
+} from 'ng-dynamic-components';
+
 import { AppComponent } from './app.component';
+import { OneComponent } from './components/one/one.component';
+import { TwoComponent } from './components/two/two.component';
+import { ThreeComponent } from './components/three/three.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, NgDynamicComponentsModule.withComponents([OneComponent, TwoComponent, ThreeComponent])],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: componentRegistryInitializerFactory,
+      multi: true,
+      deps: [ComponentRegistryService, DynamicComponents]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
