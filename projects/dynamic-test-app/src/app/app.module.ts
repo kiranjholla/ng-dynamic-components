acc34fc257cec416365f5d1ecb58ce2dbb965a96
Kiran J. Holla <kjholla@gmail.com>
2021-02-02 12:02:14 +0530

    Using RegistryModule for defining Dynamic Registry

--- projects/dynamic-test-app/src/app/app.module.ts
+++ projects/dynamic-test-app/src/app/app.module.ts
@@ -2,28 +2,23 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {
-  componentRegistryInitializerFactory,
+  registryInitializerFactory,
   ComponentRegistryService,
-  DynamicComponents,
   NgDynamicComponentsModule
 } from 'ng-dynamic-components';
 
 import { AppComponent } from './app.component';
-import { OneComponent } from './components/one/one.component';
-import { ThreeComponent } from './components/three/three.component';
-import { TwoComponent } from './components/two/two.component';
-
-
+import { RegistryModule } from './registry/registry.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgDynamicComponentsModule.withComponents({ OneComponent, TwoComponent, ThreeComponent })],
+  imports: [BrowserModule, NgDynamicComponentsModule.withRegistry(RegistryModule)],
   providers: [
     {
       provide: APP_INITIALIZER,
-      useFactory: componentRegistryInitializerFactory,
+      useFactory: registryInitializerFactory,
       multi: true,
-      deps: [ComponentRegistryService, DynamicComponents]
+      deps: [ComponentRegistryService]
     }
   ],
   bootstrap: [AppComponent]
